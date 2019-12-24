b7c117f67b6f4161853e1f2c0f1e82478d688275
pakeesan <sanpakees@gmail.com>
2019-12-24 11:14:29 +0530

    projectid is unique for release

--- sgic-internal-services/defecttracker-service/src/main/java/com/sgic/internal/defecttracker/defectservice/entities/Release.java
+++ sgic-internal-services/defecttracker-service/src/main/java/com/sgic/internal/defecttracker/defectservice/entities/Release.java
@@ -12,20 +12,44 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
-@Table(schema="defectservices",name = "release")
+
+@Table(schema="defectservices",name = "release", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {
+            "projectId"
+        })
+        
+})
 public class Release {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long releaseId;
 	
-	//private List<String> releaseName;
+	
 	@JsonFormat(with = JsonFormat.Feature.ACCEPT_SINGLE_VALUE_AS_ARRAY)
 	private ArrayList<String> releaseName;
+	
+	@ManyToOne
+	@JoinColumn(name="projectId",nullable=false)
+	private Project project;
+
+	
+	
+	
+	
+	
+	public Long getReleaseId() {
+		return releaseId;
+	}
+
+	public void setReleaseId(Long releaseId) {
+		this.releaseId = releaseId;
+	}
 	
 	public ArrayList<String> getReleaseName() {
 		return releaseName;
@@ -35,25 +59,7 @@ public class Release {
 		this.releaseName = releaseName;
 	}
 
-	@ManyToOne
-	@JoinColumn(name="projectId",nullable=false)
-	private Project project;
 
-	public Long getReleaseId() {
-		return releaseId;
-	}
-
-	public void setReleaseId(Long releaseId) {
-		this.releaseId = releaseId;
-	}
-
-//	public List<String> getReleaseName() {
-//		return releaseName;
-//	}
-//
-//	public void setReleaseName(List<String> releaseName) {
-//		this.releaseName = releaseName;
-//	}
 
 	public Project getProject() {
 		return project;
--- sgic-internal-services/defecttracker-service/src/main/java/com/sgic/internal/defecttracker/defectservice/repositories/ReleaseRepository.java
+++ sgic-internal-services/defecttracker-service/src/main/java/com/sgic/internal/defecttracker/defectservice/repositories/ReleaseRepository.java
@@ -12,13 +12,7 @@ public interface ReleaseRepository extends JpaRepository<Release, Long> {
 	
 	Release getByReleaseId(Long releaseId);
 	
-//	String FINDRELEASE= "SELECT * FROM release where project_id=:projectId";
-//	@Query(value=FINDRELEASE ,nativeQuery=true)
-//	Release getrelease(Long projectId);
-	
-////	String FINDRELEASE= "SELECT * FROM release where project_id=?1";
-//	@Query(value="SELECT * FROM Release where project_id =: projectId")
-//	Release getrelease(@Param("projectId")Long projectId);
+
 	
 	@Query(value = "from Release where project_id=:projectId")
 	List<Release> getReleaseByProjectId(@Param("projectId") Long projectId);
